83eba72a92898dc0864fff568c71a891da1e2d41
Raul1992gt <rgarciat94@gmail.com>
2022-11-21 10:34:38 +0100

    Actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LsitarJuegosComponent } from './components/lsitar-juegos/lsitar-juegos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LsitarJuegosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lsitar-juegos/lsitar-juegos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lsitar-juegos',
+  templateUrl: './lsitar-juegos.component.html',
+  styleUrls: ['./lsitar-juegos.component.css']
+})
+export class LsitarJuegosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
