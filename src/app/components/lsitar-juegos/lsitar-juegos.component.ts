f8474901be5807e82f351ec43fb4db080066fa3d
Raul1992gt <rgarciat94@gmail.com>
2022-11-21 11:08:03 +0100

    components

--- src/app/components/lsitar-juegos/lsitar-juegos.component.ts
+++ src/app/components/lsitar-juegos/lsitar-juegos.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LsitarJuegosComponent implements OnInit {
 
-  constructor() { }
+  /*Propiedades */
+  id: number;
+  title: string;
+  company: string;
+  cover: string;
+  rating: number;
+  
+  
+  constructor() {
+    this.id = 1;
+    this.title = "Call of Duty";
+    this.company = "microsoft";
+    this.cover = "imagen";
+    this.rating = 7.8;
+   }
 
   ngOnInit(): void {
   }
