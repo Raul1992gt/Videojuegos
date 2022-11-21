6aef51dc1bccc4429cb594207f7b96c1a401757d
Raul1992gt <rgarciat94@gmail.com>
2022-11-21 16:50:10 +0100

    Actualizado, no funciona desplegable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import videojuegos from 'src/assets/data/videojuegos.json'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  Videojuegos: any = videojuegos;
+  
   constructor() { }
 
   ngOnInit(): void {
